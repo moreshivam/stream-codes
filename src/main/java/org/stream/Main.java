c473e781dbe6a065ce0ac2c098e658f2395f56cf
Shivam More <moreshivam2010@gmail.com>
2026-05-01 15:23:52 +0530

    filter code

---
+++ src/main/java/org/stream/Main.java
@@ -0,0 +1,19 @@
+package org.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
+
+        List<Integer> ans = list.stream()
+                .filter(n -> n%2 ==0)
+                .collect(Collectors.toList());
+
+        System.out.println(ans);
+    }
+}
