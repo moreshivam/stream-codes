23699a9fb8287b29848a3cfcffa76063ba48d5b2
Shivam More <moreshivam2010@gmail.com>
2026-05-02 10:38:24 +0530

    2nd largest element

--- src/main/java/org/stream/Main.java
+++ src/main/java/org/stream/Main.java
@@ -1,5 +1,6 @@
 package org.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +11,22 @@ public class Main {
 
         List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
 
+       //find all even numbers
         List<Integer> ans = list.stream()
                 .filter(n -> n%2 ==0)
                 .collect(Collectors.toList());
 
         System.out.println(ans);
+
+        //find the second largest number
+        int ans2 = list.stream()
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .skip(1)
+                .findFirst()
+                .orElse(-1);
+
+        System.out.println(ans2);
+
     }
 }
